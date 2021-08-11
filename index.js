34198883976c096afe6090849367d56a3b257749
tharvey <tharveyster@gmail.com>
2021-08-11 11:30:13 -0400

    Change to arrow functions and moved errors to if/else statements

--- index.js
+++ index.js
@@ -12,24 +12,33 @@ const db = mysql.createConnection(
     console.log(`Connected to the employee_db database.`)
 );
 
-function viewAllDepartments() {
+const viewAllDepartments = () => {
     db.query('SELECT name AS "DEPARTMENT NAME", id AS ID FROM department', function(err, results) {
-        console.log(err);
-        console.table(results);
+        if (err) {
+            console.log(err);
+        } else {
+            console.table(results);
+        }
     });
 }
 
-function viewAllRoles() {
+const viewAllRoles = () => {
     db.query('SELECT a.title AS TITLE, a.id AS ID, b.name AS "DEPARTMENT", a.salary AS SALARY FROM role a JOIN department b ON a.department_id = b.id', function(err, results) {
-        console.log(err);
-        console.table(results);
+        if (err) {
+            console.log(err);
+        } else {
+            console.table(results);
+        }
     })
 }
 
-function viewAllEmployees() {
+const viewAllEmployees = () => {
     db.query('SELECT a.id AS ID, a.first_name AS "FIRST NAME", a.last_name AS "LAST NAME", b.title AS "JOB TITLE", c.name AS DEPARTMENT, b.salary AS SALARY, CONCAT(d.first_name, " ", d.last_name) AS MANAGER FROM employee a JOIN role b ON a.role_id = b.id JOIN department c ON b.department_id = c.id LEFT JOIN employee d on a.manager_id = d.id', function(err, results) {
-        console.log(err);
-        console.table(results);
+        if (err) {
+            console.log(err);
+        } else {
+            console.table(results);
+        }
     })
 }
 
